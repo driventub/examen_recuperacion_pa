44d3255d5d9d49d66f8cbb03a88beb54cda8e0b7
driven <aldazpaulo@gmail.com>
2022-04-11 16:47:28 -0500

    base

---
+++ src/main/java/ec/edu/ec/examen_rec/ExamenRecApplication.java
@@ -0,0 +1,22 @@
+package ec.edu.ec.examen_rec;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class ExamenRecApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenRecApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+}
---
+++ src/test/java/ec/edu/ec/examen_rec/ExamenRecApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ec.examen_rec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenRecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
